fbd33741b2781df9c905eaee39a35241b46c8bbb
msbeigiai <msbeigi83@gmail.com>
2024-12-11 16:34:19 +0200

    Edit CategoryRepository

--- autosparepart_backend/src/main/java/com/irmazda/autosparepart/repository/CategoryRepository.java
+++ autosparepart_backend/src/main/java/com/irmazda/autosparepart/repository/CategoryRepository.java
@@ -1,8 +1,11 @@
 package com.irmazda.autosparepart.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.irmazda.autosparepart.entity.Category;
 
 public interface CategoryRepository extends JpaRepository<Category, Long> {
+  Optional<Category> findByName(String name);
 }
