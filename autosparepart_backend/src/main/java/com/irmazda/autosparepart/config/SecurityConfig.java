350c94743ada8773bc9b8ea1b9573f3eb1b294a5
msbeigiai <msbeigi83@gmail.com>
2024-12-14 17:10:22 +0200

    Edit SecurityConfig

--- autosparepart_backend/src/main/java/com/irmazda/autosparepart/config/SecurityConfig.java
+++ autosparepart_backend/src/main/java/com/irmazda/autosparepart/config/SecurityConfig.java
@@ -1,12 +1,21 @@
 package com.irmazda.autosparepart.config;
 
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.List;
 
 @Configuration
 @EnableWebSecurity
@@ -20,10 +29,34 @@ public class SecurityConfig {
                     .permitAll()
                     .anyRequest()
                     .authenticated())
+
             .oauth2ResourceServer(oauth2 -> oauth2
                     .jwt(Customizer.withDefaults())
             );
+    http.sessionManagement(
+            session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+
+    http.csrf(AbstractHttpConfigurer::disable);
+    http.cors(c -> c.configurationSource(corsConfigurationSource()));
 
     return http.build();
+  }
+
+  @Bean
+  public CorsConfigurationSource corsConfigurationSource() {
+    return new CorsConfigurationSource() {
+
+      @Override
+      public CorsConfiguration getCorsConfiguration(HttpServletRequest request) {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOrigins(List.of("http://localhost:5173"));
+        configuration.setAllowedMethods(List.of("*"));
+        configuration.setAllowCredentials(true);
+        configuration.setAllowedHeaders(List.of("Content-Type", "Authorization", "Accept"));
+        configuration.setExposedHeaders(List.of("Authorization"));
+        configuration.setMaxAge(3600L);
+        return configuration;
+      }
+    };
   }
 }
