2d68ff6f7ca78211a45460e7c46d8f8fbd841f8d
msbeigiai <msbeigi83@gmail.com>
2024-12-17 19:07:21 +0200

    Edit UserService

--- autosparepart_backend/src/main/java/com/irmazda/autosparepart/service/UserService.java
+++ autosparepart_backend/src/main/java/com/irmazda/autosparepart/service/UserService.java
@@ -2,9 +2,12 @@ package com.irmazda.autosparepart.service;
 
 import com.irmazda.autosparepart.entity.User;
 import com.irmazda.autosparepart.repository.UserRepository;
+import org.keycloak.KeycloakPrincipal;
 import org.springframework.stereotype.Service;
 
+
 import java.security.Principal;
+import java.util.UUID;
 
 @Service
 public class UserService {
@@ -14,9 +17,12 @@ public class UserService {
     this.userRepository = userRepository;
   }
 
-
   public User getUserFromPrincipal(Principal principal) {
-    // Implement user fetching logic
-    return new User(); // Placeholder
+    if (principal instanceof KeycloakPrincipal<?> keycloakPrincipal) {
+      String userId = keycloakPrincipal.getKeycloakSecurityContext().getToken().getSubject();
+      return userRepository.findById(UUID.fromString(userId))
+              .orElseThrow(() -> new RuntimeException("User not found"));
+    }
+    throw new RuntimeException("Invalid Principal type");
   }
 }
