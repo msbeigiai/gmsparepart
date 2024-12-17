480b55307e418ae56c1b0bb06d6e81df4a691312
msbeigiai <msbeigi83@gmail.com>
2024-12-17 19:07:30 +0200

    Edit KeycloakRoleConverter

--- autosparepart_backend/src/main/java/com/irmazda/autosparepart/config/KeycloakRoleConverter.java
+++ autosparepart_backend/src/main/java/com/irmazda/autosparepart/config/KeycloakRoleConverter.java
@@ -4,24 +4,26 @@ import org.springframework.core.convert.converter.Converter;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.oauth2.jwt.Jwt;
-import org.springframework.security.oauth2.server.resource.authentication.JwtGrantedAuthoritiesConverter;
 
 import java.util.Collection;
 import java.util.List;
+import java.util.Map;
 import java.util.stream.Collectors;
 
 public class KeycloakRoleConverter implements Converter<Jwt, Collection<GrantedAuthority>> {
-  private final JwtGrantedAuthoritiesConverter converter = new JwtGrantedAuthoritiesConverter();
 
   @Override
   public Collection<GrantedAuthority> convert(Jwt jwt) {
-    var authorities = converter.convert(jwt);
+    Map<String, Object> realmAccess = jwt.getClaim("realm_access");
 
-    List<GrantedAuthority> realmRoles =
-            ((List<String>) jwt.getClaimAsMap("realm_access").get("roles")).stream()
-                    .map(role -> "ROLE_" + role).map(SimpleGrantedAuthority::new)
-                    .collect(Collectors.toList());
-    authorities.addAll(realmRoles);
-    return authorities;
+    if (realmAccess == null || realmAccess.isEmpty()) {
+      return List.of();
+    }
+
+    List<String> roles = (List<String>) realmAccess.get("roles");
+
+    return roles.stream()
+            .map(role -> new SimpleGrantedAuthority("ROLE_" + role.toUpperCase()))
+            .collect(Collectors.toList());
   }
 }
