aa0921b546cb0684ba5fcdba4797929820cc7892
msbeigiai <msbeigi83@gmail.com>
2024-12-18 20:18:51 +0200

    Edit KeycloakRoleConverter

--- autosparepart_backend/src/main/java/com/irmazda/autosparepart/config/KeycloakRoleConverter.java
+++ autosparepart_backend/src/main/java/com/irmazda/autosparepart/config/KeycloakRoleConverter.java
@@ -4,12 +4,14 @@ import org.springframework.core.convert.converter.Converter;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.oauth2.jwt.Jwt;
+import org.springframework.stereotype.Component;
 
 import java.util.Collection;
 import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
 
+@Component
 public class KeycloakRoleConverter implements Converter<Jwt, Collection<GrantedAuthority>> {
 
   @Override
