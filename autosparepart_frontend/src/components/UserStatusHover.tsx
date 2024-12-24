9f7c8b830222cb622dfb12b5cc9bd5bad7113991
msbeigiai <msbeigi83@gmail.com>
2024-12-24 17:48:43 +0200

    Add hover component

---
+++ autosparepart_frontend/src/components/UserStatusHover.tsx
@@ -0,0 +1,47 @@
+import { CalendarDays, User } from "lucide-react"
+
+import {
+  Avatar,
+  AvatarFallback,
+  AvatarImage,
+} from "@/components/ui/avatar"
+import { Button } from "@/components/ui/button"
+import {
+  HoverCard,
+  HoverCardContent,
+  HoverCardTrigger,
+} from "@/components/ui/hover-card"
+import { useAppSelector } from "@/app/hooks";
+
+export function UserStatusHover() {
+  const { isAuthenticated, user } = useAppSelector((state) => state.auth);
+
+  return (
+    <HoverCard>
+      <HoverCardTrigger asChild>
+        <Button variant="link">
+          <User />
+        </Button>
+      </HoverCardTrigger>
+      <HoverCardContent className="w-60">
+        <div className="flex justify-between space-x-4">
+          <Avatar>
+            <AvatarImage src="https://github.com/vercel.png" />
+            <AvatarFallback>VC</AvatarFallback>
+          </Avatar>
+          <div className="space-y-1">
+            <h4 className="text-sm font-semibold">{user?.email}</h4>
+            <div className="flex items-center pt-2">
+              <CalendarDays className="mr-2 h-4 w-4 opacity-70" />{" "}
+              <span className="text-xs text-muted-foreground">
+                Joined December 2021
+              </span>
+            </div>
+          </div>
+        </div>
+      </HoverCardContent>
+    </HoverCard>
+  )
+}
+
+export default UserStatusHover;
