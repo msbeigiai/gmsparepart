03f92d182ee598213a2ef8f1bebc68358786825f
msbeigiai <msbeigi83@gmail.com>
2024-12-31 20:06:40 +0200

    Add Address repository

---
+++ autosparepart_backend/src/main/java/com/irmazda/autosparepart/repository/AddressRepository.java
@@ -0,0 +1,15 @@
+package com.irmazda.autosparepart.repository;
+
+import java.util.List;
+import java.util.UUID;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import com.irmazda.autosparepart.entity.Address;
+
+public interface AddressRepository extends JpaRepository<Address, Long> {
+  @Query("SELECT a FROM Address a WHERE a.user.userId = ?1")
+  List<Address> findByUserId(UUID userId);
+
+}
