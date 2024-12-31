d4ef527cf426e9ff62845316884570fabd419621
msbeigiai <msbeigi83@gmail.com>
2024-12-31 20:05:46 +0200

    Edit security config

--- autosparepart_backend/src/main/java/com/irmazda/autosparepart/config/SecurityConfig.java
+++ autosparepart_backend/src/main/java/com/irmazda/autosparepart/config/SecurityConfig.java
@@ -1,11 +1,8 @@
 package com.irmazda.autosparepart.config;
 
-import jakarta.servlet.http.HttpServletRequest;
-
-import org.keycloak.authorization.client.util.Http;
+import org.eclipse.microprofile.openapi.models.PathItem.HttpMethod;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
@@ -15,8 +12,6 @@ import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
-
-import java.util.List;
 
 @Configuration
 @EnableWebSecurity
@@ -31,12 +26,9 @@ public class SecurityConfig {
   @Bean
   public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
     http.authorizeHttpRequests(authorize -> authorize
-        // .requestMatchers(HttpMethod.POST, "/api/v1/carts/add").permitAll()
-        // .requestMatchers(HttpMethod.GET, "/api/v1/products").permitAll()
-        // .requestMatchers(HttpMethod.GET, "/home").permitAll()
-        // .anyRequest().authenticated()
-        .anyRequest().permitAll()
-        )
+        .anyRequest().authenticated()
+    // .anyRequest().permitAll()
+    )
         .oauth2ResourceServer(oauth2 -> oauth2
             .jwt(jwtConfigurer -> jwtConfigurer.jwtAuthenticationConverter(jwtAuthenticationConverter())));
     http.sessionManagement(
@@ -58,7 +50,7 @@ public class SecurityConfig {
   @Bean
   public CorsConfigurationSource corsConfigurationSource() {
     CorsConfiguration configuration = new CorsConfiguration();
-    configuration.addAllowedOrigin("http://localhost:5173"); 
+    configuration.addAllowedOrigin("http://localhost:5173");
     configuration.addAllowedMethod("*");
     configuration.addAllowedHeader("*");
     configuration.setAllowCredentials(true);
