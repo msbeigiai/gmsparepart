a331972e473be4abdf782ed069c3953e90eb90b0
msbeigiai <msbeigi83@gmail.com>
2024-12-31 20:08:39 +0200

    Edit AppRouter

--- autosparepart_frontend/src/routes/AppRoutes.tsx
+++ autosparepart_frontend/src/routes/AppRoutes.tsx
@@ -1,17 +1,16 @@
-import Login from "@/components/pages/Login";
 import About from "@/components/pages/About";
 import Home from "@/components/pages/Home";
 import Profile from "@/components/pages/Profile";
 // import NotFound from "@/components/pages/NotFound";
-import { Route, Routes } from "react-router-dom";
 import CheckoutStepper from "@/components/pages/CheckoutStepper";
+import { Route, Routes } from "react-router-dom";
 
 const AppRoutes = () => (
   <Routes>
     <Route path="/" element={<Home />} />
     <Route path="/about" element={<About />} />
     <Route path="/profile" element={<Profile />} />
-    <Route path="/login" element={<Login />} />
+    {/* <Route path="/login" element={<Login />} /> */}
     <Route path="/checkout" element={<CheckoutStepper />} />
 
     {/* <Route path="*" element={<NotFound />} /> */}
