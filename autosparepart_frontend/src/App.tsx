de71e98665b169fb30c900713f5c9dd84b77e742
msbeigiai <msbeigi83@gmail.com>
2025-01-01 19:14:13 +0200

    Update App

--- autosparepart_frontend/src/App.tsx
+++ autosparepart_frontend/src/App.tsx
@@ -1,23 +1,41 @@
-import AppSidebar from './components/navigation/AppSidebar'
+import { useEffect } from 'react'
+import { useAppDispatch } from './app/hooks'
 import Navbar from './components/navigation/Navbar'
-import { SidebarProvider, SidebarTrigger } from './components/ui/sidebar'
+import { Toaster } from './components/ui/toaster'
+import { initializeKeycloak, keycloak, loginSuccess } from './features/auth/authSlice'
 import AppRoutes from './routes/AppRoutes'
+import Home from './components/pages/Home'
 
 function App() {
+  const dispatch = useAppDispatch();
+
+  useEffect(() => {
+    initializeKeycloak().then((authenticated) => {
+      console.log('Keycloak initialized: ', authenticated);
+      if(authenticated) {
+        console.log('Keycloak initialized: ', authenticated);
+        localStorage.setItem('checkoutRedirect', 'true');
+        dispatch(loginSuccess({ token: keycloak.token!, user: keycloak.tokenParsed! }));
+      }
+    }
+
+  )}, [dispatch])
+
   return (
     <div className="flex">
-      <SidebarProvider>
+      <Toaster />
+      {/* <SidebarProvider>
         <AppSidebar />
-        <SidebarTrigger />
+        <SidebarTrigger /> */}
         <div className="flex-1 flex flex-col">
           <Navbar />
           <main className="flex-1 p-4">
             <AppRoutes />
           </main>
         </div>
-      </SidebarProvider>
+      {/* </SidebarProvider> */}
     </div>
   )
 }
 
-export default App
+export default App;
