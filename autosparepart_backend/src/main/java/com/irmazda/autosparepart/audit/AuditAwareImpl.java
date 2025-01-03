0d076f9c0f0150866fe34bd87afedf430050bf9e
msbeigiai <msbeigi83@gmail.com>
2025-01-03 20:00:15 +0200

    Edit AuditAware

--- autosparepart_backend/src/main/java/com/irmazda/autosparepart/audit/AuditAwareImpl.java
+++ autosparepart_backend/src/main/java/com/irmazda/autosparepart/audit/AuditAwareImpl.java
@@ -3,12 +3,15 @@ package com.irmazda.autosparepart.audit;
 import java.util.Optional;
 
 import org.springframework.data.domain.AuditorAware;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
 
 @Component("AuditAwareImpl")
 public class AuditAwareImpl<T> implements AuditorAware<String> {
   @Override
   public Optional<String> getCurrentAuditor() {
-    return Optional.of("test");
+    SecurityContext context = SecurityContextHolder.getContext();
+    return Optional.of(context.getAuthentication().getName());
   }
 }
