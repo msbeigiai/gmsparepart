a8424ac743541195fb0263a50d5b73cd58de902e
msbeigiai <msbeigi83@gmail.com>
2025-01-10 19:24:30 +0200

    Edit SecurityConfig

--- autosparepart_backend/src/main/java/com/irmazda/autosparepart/config/SecurityConfig.java
+++ autosparepart_backend/src/main/java/com/irmazda/autosparepart/config/SecurityConfig.java
@@ -27,6 +27,7 @@ public class SecurityConfig {
   public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
     http.authorizeHttpRequests(authorize -> authorize
         .requestMatchers(HttpMethod.GET, "/api/v1/products/**").permitAll()
+        .requestMatchers(HttpMethod.GET, "/api/v1/reviews/**").permitAll()
         .anyRequest().authenticated()
     // .anyRequest().permitAll()
     )
