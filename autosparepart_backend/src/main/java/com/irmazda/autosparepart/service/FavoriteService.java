550eb1faa267ddb47dfaf1fb5804aad9d91b2b38
msbeigiai <msbeigi83@gmail.com>
2025-01-10 19:24:07 +0200

    Refactor FavoriteService

--- autosparepart_backend/src/main/java/com/irmazda/autosparepart/service/FavoriteService.java
+++ autosparepart_backend/src/main/java/com/irmazda/autosparepart/service/FavoriteService.java
@@ -5,10 +5,11 @@ import java.util.List;
 import java.util.UUID;
 
 import com.irmazda.autosparepart.dto.favorite.FavoriteDTO;
-import com.irmazda.autosparepart.entity.Favorite;
 
 public interface FavoriteService {
   List<FavoriteDTO> getUserFavorites(Principal principal);
-  Favorite addFavorite(UUID productId, Principal principal);
+
+  void addFavorite(UUID productId, Principal principal);
+
   void removeFavorite(UUID productId, Principal principal);
 }
