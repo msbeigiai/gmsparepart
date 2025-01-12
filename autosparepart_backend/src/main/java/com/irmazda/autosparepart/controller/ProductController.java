cd082c3845f03007aaf0dc54b278208ee309b7ee
msbeigiai <msbeigi83@gmail.com>
2025-01-12 21:17:06 +0200

    Add ProductController

--- autosparepart_backend/src/main/java/com/irmazda/autosparepart/controller/ProductController.java
+++ autosparepart_backend/src/main/java/com/irmazda/autosparepart/controller/ProductController.java
@@ -1,8 +1,7 @@
 package com.irmazda.autosparepart.controller;
 
 import com.irmazda.autosparepart.dto.product.ProductDTO;
-import com.irmazda.autosparepart.dto.product.ProductRequestDTO;
-import com.irmazda.autosparepart.dto.product.ProductResponseDTO;
+import com.irmazda.autosparepart.dto.product.ProductRequest;
 import com.irmazda.autosparepart.service.ProductService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -22,24 +21,34 @@ public class ProductController {
   }
 
   @PostMapping
-  public ResponseEntity<ProductDTO> addProduct(@RequestBody ProductRequestDTO pRequestDTO) {
+  public ResponseEntity<ProductDTO> addProduct(@RequestBody ProductRequest pRequestDTO) {
     return ResponseEntity
             .status(HttpStatus.CREATED)
             .body(productService.addProduct(pRequestDTO));
   }
 
   @GetMapping
-  public ResponseEntity<List<ProductResponseDTO>> getAllProducts() {
+  public ResponseEntity<List<ProductDTO>> getAllProducts() {
     return ResponseEntity
             .status(HttpStatus.OK)
             .body(productService.getAllProducts());
   }
 
   @GetMapping("/{productId}")
-  public ResponseEntity<ProductResponseDTO> getProductById(@PathVariable UUID productId) {
+  public ResponseEntity<ProductDTO> getProductById(@PathVariable UUID productId) {
     return ResponseEntity
             .status(HttpStatus.OK)
             .body(productService.getProductById(productId));
+  }
+
+  @GetMapping("/{categoryId}/count")
+  public ResponseEntity<Long> getProductCategoryCount(@PathVariable Long categoryId) {
+    return ResponseEntity.ok(productService.getProductCategoryCount(categoryId));
+  }
+
+  @GetMapping("/category/{categoryId}")
+  public ResponseEntity<List<ProductDTO>> getProductsByCategory(@PathVariable Long categoryId) {
+    return ResponseEntity.ok(productService.getProductsByCategory(categoryId));
   }
 
 }
