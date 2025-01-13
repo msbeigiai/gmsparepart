3ff660cb5ce2f644ff46d0c899a8c359dee2bdda
msbeigiai <msbeigi83@gmail.com>
2025-01-13 18:22:08 +0200

    Edit productSlice

--- autosparepart_frontend/src/features/products/productsSlice.ts
+++ autosparepart_frontend/src/features/products/productsSlice.ts
@@ -1,13 +1,7 @@
+import { Product } from "@/types";
 import { createAsyncThunk, createSlice, PayloadAction } from "@reduxjs/toolkit";
 import axios from "axios";
 
-interface Product {
-  id: number;
-  name: string;
-  price: number;
-  description: string;
-  image: string;
-}
 
 interface ProductsState {
   items: Product[];
@@ -15,24 +9,46 @@ interface ProductsState {
   error: string | null;
 }
 
-const API_BASE_URL = 'http://localhost:8081/api/v1/products';
-
-export const fetchProducts = createAsyncThunk('products/fetchProducts', async (_, { getState }) => {
-  const state: any = getState();  
-  const token = state.auth.token;
-  const response = await axios.get<Product[]>(API_BASE_URL, {
-    headers: {
-      Authorization: `Bearer ${token}`
-    }
-  });
-  return response.data;
-});
-
 const initialState: ProductsState = {
   items: [],
   status: 'idle',
   error: null
 };
+const API_BASE_URL = 'http://localhost:8081/api/v1/products';
+
+export const fetchProducts = createAsyncThunk('products/fetchProducts', async (_, { getState }) => {
+  // const state: any = getState();
+  // const token = state.auth.token;
+  // console.log("TOKEN: ", token);
+  const response = await axios.get<Product[]>(API_BASE_URL, {
+    // headers: {
+    //   Authorization: `Bearer ${token}`
+    // }
+  });
+  return response.data;
+});
+
+export const getProductCategoryCount = createAsyncThunk("categories/getProducts", 
+  async ({ categoryId }: { categoryId: number }) => {
+  const response = await axios.get<number>(`${API_BASE_URL}/${categoryId}/count`);
+  return response.data;
+});
+
+export const getProductsByCategory = createAsyncThunk("categories/getProductsByCategory", 
+  async ({ categoryId }: { categoryId: number }) => {
+  const response = await axios.get<Product[]>(`${API_BASE_URL}/category/${categoryId}`);
+  return response.data;
+});
+
+export const fetchProductsByCategoryIds = createAsyncThunk("categories/getProductsByCategoryIds", 
+  async ({ categoryIds }: { categoryIds: string[] }) => {
+  const response = await axios.get<Product[]>(`${API_BASE_URL}/categories`, {
+    params: {
+      categoryIds: categoryIds.join(","),
+    }
+  });
+  return response.data;
+});
 
 const productSlice = createSlice({
   name: 'products',
@@ -48,6 +64,28 @@ const productSlice = createSlice({
         state.items = action.payload;
       })
       .addCase(fetchProducts.rejected, (state, action) => {
+        state.status = 'failed';
+        state.error = action.error.message || 'Error fetching products';
+      })
+      .addCase(getProductsByCategory.pending, (state) => {
+        state.status = 'loading';
+      })
+      .addCase(getProductsByCategory.fulfilled, (state, action: PayloadAction<Product[]>) => {
+        state.status = 'succeeded';
+        state.items = action.payload;
+      })
+      .addCase(getProductsByCategory.rejected, (state, action) => {
+        state.status = 'failed';
+        state.error = action.error.message || 'Error fetching products';
+      })
+      .addCase(fetchProductsByCategoryIds.pending, (state) => {
+        state.status = 'loading';
+      })
+      .addCase(fetchProductsByCategoryIds.fulfilled, (state, action: PayloadAction<Product[]>) => {
+        state.status = 'succeeded';
+        state.items = action.payload;
+      })
+      .addCase(fetchProductsByCategoryIds.rejected, (state, action) => {
         state.status = 'failed';
         state.error = action.error.message || 'Error fetching products';
       })
