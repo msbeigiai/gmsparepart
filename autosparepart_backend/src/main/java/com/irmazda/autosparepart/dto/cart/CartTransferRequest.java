d4a13d483824b9bee401f711192c91c6bfd067d4
msbeigiai <msbeigi83@gmail.com>
2025-01-23 18:35:18 +0200

    Add CartTransferRequest

---
+++ autosparepart_backend/src/main/java/com/irmazda/autosparepart/dto/cart/CartTransferRequest.java
@@ -0,0 +1,38 @@
+package com.irmazda.autosparepart.dto.cart;
+
+import java.util.List;
+import java.util.UUID;
+
+public class CartTransferRequest {
+
+  private List<CartItemDTO> cartItems;
+
+  public List<CartItemDTO> getCartItems() {
+    return cartItems;
+  }
+
+  public void setCartItems(List<CartItemDTO> cartItems) {
+    this.cartItems = cartItems;
+  }
+
+  public static class CartItemDTO {
+    private UUID productId;
+    private int quantity;
+
+    public UUID getProductId() {
+      return productId;
+    }
+
+    public void setProductId(UUID productId) {
+      this.productId = productId;
+    }
+
+    public int getQuantity() {
+      return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+      this.quantity = quantity;
+    }
+  }
+}
