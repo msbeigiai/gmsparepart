cebdd484bdd3833c2588794d5b399506e8922ec0
msbeigiai <msbeigi83@gmail.com>
2025-02-10 17:56:39 +0200

    Edit AppRoute

--- autosparepart_frontend/src/routes/AppRoutes.tsx
+++ autosparepart_frontend/src/routes/AppRoutes.tsx
@@ -3,19 +3,19 @@ import Home from "@/components/pages/Home";
 import Profile from "@/components/pages/Profile";
 // import NotFound from "@/components/pages/NotFound";
 import CheckoutStepper from "@/components/pages/CheckoutStepper";
-import { Route, Routes } from "react-router-dom";
-import ProductDetails from "@/components/pages/ProductDetails";
+import Order from "@/components/pages/Order";
+import SampleProductDetails from "@/components/pages/SampleProductDetails";
 import Products from "@/components/Products";
+import { Route, Routes } from "react-router-dom";
 
 const AppRoutes = () => (
   <Routes>
     <Route path="/" element={<Home />} />
     <Route path="/about" element={<About />} />
     <Route path="/profile" element={<Profile />} />
-    <Route path="/products/:productId" element={<ProductDetails />} />
+    <Route path="/products/:productId" element={<SampleProductDetails />} />
     <Route path="/products" element={<Products />} />
-
-    {/* <Route path="/login" element={<Login />} /> */}
+    <Route path="/orders/:orderId" element={<Order />} />
     <Route path="/checkout" element={<CheckoutStepper />} />
 
     {/* <Route path="*" element={<NotFound />} /> */}
