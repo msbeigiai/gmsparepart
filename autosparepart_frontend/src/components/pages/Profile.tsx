bedd3eb6d72f524828ef893fd7d3ddb46352f7ed
msbeigiai <msbeigi83@gmail.com>
2025-02-10 17:56:50 +0200

    Edit Profile

--- autosparepart_frontend/src/components/pages/Profile.tsx
+++ autosparepart_frontend/src/components/pages/Profile.tsx
@@ -1,64 +1,35 @@
 import { useAppDispatch, useAppSelector } from "@/app/hooks";
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 import { Button } from "@/components/ui/button";
-import {
-  Card,
-  CardContent,
-  CardFooter,
-  CardHeader,
-  CardTitle,
-} from "@/components/ui/card";
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
-import {
-  addAddress,
-  deleteAddress,
-  fetchAddresses,
-  updateAddress,
-} from "@/features/address/addressSlice";
+import { fetchAddresses } from "@/features/address/addressSlice";
 import { keycloak } from "@/features/auth/authSlice";
 import { fetchFavorites } from "@/features/favorite/favoriteSlice";
 import { fetchAllOrders } from "@/features/order/orderSlice";
 import { fetchUserProfile } from "@/features/user/userSlice";
-import { AddAddressRequest } from "@/types";
-import {
-  Bell,
-  CreditCard,
-  ExternalLink,
-  Heart,
-  Package,
-  Plus,
-  ShoppingBag,
-} from "lucide-react";
-import { useEffect, useRef, useState } from "react";
+import { Bell, CreditCard, Heart, Plus, ShoppingBag } from "lucide-react";
+import { useEffect } from "react";
 import { Link } from "react-router-dom";
-import AddressForm from "../AddressForm";
-import {
-  Collapsible,
-  CollapsibleContent,
-  CollapsibleTrigger,
-} from "../ui/collapsible";
-import OrderStatusBadge from "./OrderStatusBadge";
+import ProfileAddressesList from "../ProfileAddressesList";
+import ProfileOrdersList from "../ProfileOrdersList";
 
-export enum OrderStatus {
-  COMPLETED = "COMPLETED",
-  PENDING = "PENDING",
-  DELIVERED = "DELIVERED",
-  PROCESSING = "PROCESSING",
-}
+// export enum OrderStatus {
+//   COMPLETED = "COMPLETED",
+//   PENDING = "PENDING",
+//   DELIVERED = "DELIVERED",
+//   PROCESSING = "PROCESSING",
+// }
 
 const EcommerceProfile = () => {
   const dispatch = useAppDispatch();
-  const [showForm, setShowForm] = useState(false);
   const { items: addresses } = useAppSelector((state) => state.addresses);
   const { isAuthenticated } = useAppSelector((state) => state.auth);
   const { items: favorites } = useAppSelector((state) => state.favorites);
   const { item: userProfile } = useAppSelector((state) => state.users);
   const { items: orders } = useAppSelector((state) => state.order);
-  const [editingAddress, setEditingAddress] =
-    useState<AddAddressRequest | null>(null);
-  const formRef = useRef<{ reset: () => void }>(null);
 
   useEffect(() => {
     if (isAuthenticated) {
@@ -70,48 +41,6 @@ const EcommerceProfile = () => {
   }, [dispatch, isAuthenticated, addresses.length, orders.length]);
 
   console.log("ORDERS: ", orders);
-
-  const handleDeleteAddress = (addressId: number) => {
-    dispatch(deleteAddress({ addressId }));
-  };
-
-  const handleFormSubmit = (values: AddAddressRequest) => {
-    if (editingAddress) {
-      console.log("EDIT ADDRESS: ", editingAddress);
-
-      dispatch(
-        updateAddress({
-          addressId: editingAddress.addressId!,
-          address: { ...values },
-        })
-      );
-      setEditingAddress(null);
-    } else {
-      dispatch(addAddress({ request: values }));
-    }
-    formRef.current?.reset();
-    setShowForm(false);
-  };
-
-  const handelFormCancel = () => {
-    setEditingAddress(null);
-    formRef.current?.reset();
-    setShowForm(false);
-  };
-
-  if (editingAddress) {
-    console.log("EDIT ADDRESS: ", editingAddress);
-
-    return (
-      <AddressForm
-        initialValues={editingAddress}
-        onSubmit={handleFormSubmit}
-        onCancel={handelFormCancel}
-        isEditing={true}
-        formRef={formRef}
-      />
-    );
-  }
 
   if (!isAuthenticated) {
     return (
@@ -196,132 +125,14 @@ const EcommerceProfile = () => {
                 </div>
               </CardHeader>
               <CardContent>
-                {orders.length > 0 &&
-                  orders.map((order) => (
-                    <div
-                      key={order.orderId}
-                      className="border-b last:border-0 py-4"
-                    >
-                      <div className="flex items-center justify-between">
-                        <div className="flex items-center space-x-4">
-                          <div className="h-16 w-16 bg-gray-100 rounded-lg flex items-center justify-center">
-                            <Package className="h-8 w-8 text-gray-500" />
-                          </div>
-                          <div>
-                            <p className="font-medium">
-                              Order #{order.orderId.slice(0, 8).toUpperCase()}
-                            </p>
-                            <p className="text-sm text-gray-600">
-                              Placed on {order.creationDate}, 2024
-                            </p>
-                            <OrderStatusBadge
-                              orderStatus={
-                                OrderStatus[
-                                  order.orderStatus as keyof typeof OrderStatus
-                                ]
-                              }
-                              size="Small"
-                            />
-                          </div>
-                        </div>
-                        <div className="text-right">
-                          <p className="font-medium">
-                            ${order.totalOrderAmount}
-                          </p>
-                          <Link to={`/orders/${order.orderId}`}>
-                            <Button variant="ghost" size="sm" className="mt-2">
-                              View Details
-                              <ExternalLink className="h-4 w-4 ml-2" />
-                            </Button>
-                          </Link>
-                        </div>
-                      </div>
-                    </div>
-                  ))}
+                <ProfileOrdersList orders={orders} />
               </CardContent>
             </Card>
           </TabsContent>
 
           {/* Addresses Tab */}
           <TabsContent value="addresses">
-            {addresses.length === 0 ? (
-              <div className="flex justify-center text-lg text-slate-400 p-4">
-                No addresses added
-              </div>
-            ) : (
-              <div className="grid grid-cols-1 md:grid-cols-1 gap-6">
-                {" "}
-                <div className="grid grid-cols-1 md:grid-cols-1 gap-6">
-                  {addresses.map((address) => (
-                    <Card key={address.addressId}>
-                      <CardContent className="p-6">
-                        <div className="flex justify-between items-start">
-                          <div>
-                            <h3 className="font-semibold mb-2">
-                              {address.addressId === 1 ? "Home" : ""}
-                              {address.default ? (
-                                <span className="ml-0 text-md bg-blue-100 text-blue-800 px-2 py-1 rounded-md">
-                                  Default
-                                </span>
-                              ) : (
-                                ""
-                              )}
-                            </h3>
-                            <p className="text-gray-600">
-                              Address Line: {address.addressLine1}
-                            </p>
-                            <p className="text-gray-600">
-                              City: {address.city}
-                            </p>
-                            <p className="text-gray-600">
-                              Country: {address.country}
-                            </p>
-                            <p className="text-gray-600">
-                              Postal Code: {address.postalCode}
-                            </p>
-                          </div>
-                        </div>
-                      </CardContent>
-                      <CardFooter className="flex justify-end space-x-2">
-                        <Button
-                          variant="outline"
-                          onClick={() => {
-                            setEditingAddress(address);
-                            setShowForm(true);
-                          }}
-                        >
-                          Edit
-                        </Button>
-                        <Button
-                          variant="outline"
-                          onClick={() => handleDeleteAddress(address.addressId)}
-                        >
-                          Delete
-                        </Button>
-                      </CardFooter>
-                    </Card>
-                  ))}
-                </div>
-              </div>
-            )}
-            <div className="pt-2 pb-2">
-              <Collapsible open={showForm} onOpenChange={setShowForm}>
-                <CollapsibleTrigger asChild>
-                  <Button className="w-full">
-                    {showForm ? "Hide Form" : "Add New Address"}
-                  </Button>
-                </CollapsibleTrigger>
-                <CollapsibleContent className="mt-4">
-                  <AddressForm
-                    initialValues={editingAddress || undefined}
-                    onSubmit={handleFormSubmit}
-                    onCancel={handelFormCancel}
-                    isEditing={!!editingAddress}
-                    formRef={formRef}
-                  />
-                </CollapsibleContent>
-              </Collapsible>
-            </div>
+            <ProfileAddressesList addresses={addresses} />
           </TabsContent>
 
           {/* Payment Methods Tab */}
@@ -376,6 +187,7 @@ const EcommerceProfile = () => {
                           <img
                             src={favorite.productImageUrl}
                             className="mb-2"
+                            alt="picture"
                           />
                           <h3 className="font-semibold">
                             Product Name: {favorite.productName}
