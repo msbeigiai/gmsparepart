705edf3e7a343d2129a786c9824631fe3b241b6b
msbeigiai <msbeigi83@gmail.com>
2025-02-14 15:20:17 +0200

    Add DTO

---
+++ autosparepart_backend/src/main/java/com/irmazda/autosparepart/dto/product/GetProductDTO.java
@@ -0,0 +1,32 @@
+package com.irmazda.autosparepart.dto.product;
+
+import java.math.BigDecimal;
+import java.util.List;
+import java.util.UUID;
+
+public class GetProductDTO extends AddProductDTO {
+  private List<String> imageUrls;
+
+  public GetProductDTO() {
+  }
+
+  public GetProductDTO(UUID productId,
+                       String name,
+                       String description,
+                       BigDecimal price,
+                       int stockQuantity,
+                       Long categoryId,
+                       String categoryName,
+                       List<String> imageUrls) {
+    super(productId, name, description, price, stockQuantity, categoryId, categoryName);
+    this.imageUrls = imageUrls;
+  }
+
+  public List<String> getImageUrls() {
+    return imageUrls;
+  }
+
+  public void setImageUrls(List<String> imageUrls) {
+    this.imageUrls = imageUrls;
+  }
+}
