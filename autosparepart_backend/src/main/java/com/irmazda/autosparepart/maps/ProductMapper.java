bfca4e20982116a963820407356348381b36ee7b
msbeigiai <msbeigi83@gmail.com>
2025-02-17 20:10:26 +0200

    Edit ProductMapper

--- autosparepart_backend/src/main/java/com/irmazda/autosparepart/maps/ProductMapper.java
+++ autosparepart_backend/src/main/java/com/irmazda/autosparepart/maps/ProductMapper.java
@@ -1,21 +1,41 @@
 package com.irmazda.autosparepart.maps;
 
-import com.irmazda.autosparepart.dto.product.ProductDTO;
+import com.irmazda.autosparepart.dto.product.AddProductDTO;
+import com.irmazda.autosparepart.dto.product.GetProductDTO;
 import com.irmazda.autosparepart.entity.Product;
+import com.irmazda.autosparepart.entity.ProductImage;
 import org.springframework.stereotype.Component;
 
 @Component
 public class ProductMapper {
-  public ProductDTO mapTo(Product product) {
-    ProductDTO productDTO = new ProductDTO();
-    productDTO.setProductId(product.getProductId());
-    productDTO.setDescription(product.getDescription());
-    productDTO.setName(product.getName());
-    productDTO.setPrice(product.getPrice());
-    productDTO.setStockQuantity(product.getStockQuantity());
-    productDTO.setImage(product.getImages().getFirst().getUrl());
-    productDTO.setCategoryId(product.getCategory().getCategoryId());
-    productDTO.setCategoryName(product.getCategory().getName());
-    return productDTO;
+  public AddProductDTO mapToAddProductDTO(Product product) {
+    AddProductDTO addProductDTO = new AddProductDTO();
+    addProductDTO.setProductId(product.getProductId());
+    addProductDTO.setDescription(product.getDescription());
+    addProductDTO.setName(product.getName());
+    addProductDTO.setPrice(product.getPrice());
+    addProductDTO.setStockQuantity(product.getStockQuantity());
+    addProductDTO.setCategoryId(product.getCategory().getCategoryId());
+    addProductDTO.setCategoryName(product.getCategory().getName());
+    addProductDTO.setBrand(product.getBrand());
+    addProductDTO.setManufacturer(product.getManufacturer());
+    addProductDTO.setCompatibility(product.getCompatibility());
+    return addProductDTO;
+  }
+
+  public GetProductDTO mapToGetProductDTO(Product product) {
+    GetProductDTO getProductDTO = new GetProductDTO();
+    getProductDTO.setProductId(product.getProductId());
+    getProductDTO.setDescription(product.getDescription());
+    getProductDTO.setName(product.getName());
+    getProductDTO.setPrice(product.getPrice());
+    getProductDTO.setStockQuantity(product.getStockQuantity());
+    getProductDTO.setCategoryId(product.getCategory().getCategoryId());
+    getProductDTO.setCategoryName(product.getCategory().getName());
+    getProductDTO.setBrand(product.getBrand());
+    getProductDTO.setManufacturer(product.getManufacturer());
+    getProductDTO.setCompatibility(product.getCompatibility());
+    getProductDTO.setImageUrls(product.getImages().stream().map(ProductImage::getUrl).toList());
+    return getProductDTO;
   }
 }
