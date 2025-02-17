bc136c6cae93a1045b12eb53ab068ebe981698ed
msbeigiai <msbeigi83@gmail.com>
2025-02-17 20:06:39 +0200

    Add AdminProductService

---
+++ autosparepart_backend/src/main/java/com/irmazda/autosparepart/service/AdminProductService.java
@@ -0,0 +1,17 @@
+package com.irmazda.autosparepart.service;
+
+import com.irmazda.autosparepart.dto.product.AddProductDTO;
+import com.irmazda.autosparepart.dto.product.BulkImportResponse;
+import com.irmazda.autosparepart.dto.product.ProductCreateRequest;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.UUID;
+
+public interface AdminProductService {
+  List<String> uploadImages(UUID productId, List<MultipartFile> files) throws IOException;
+  BulkImportResponse importProductsFromExcel(MultipartFile file);
+  AddProductDTO addProduct(ProductCreateRequest productCreateRequestDTO);
+
+}
