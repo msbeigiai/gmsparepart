b400e82e9ff3cbdbb0ad93b3c933d3fb37563f98
msbeigiai <msbeigi83@gmail.com>
2025-02-17 20:11:28 +0200

    Edit ProductImageRepository

--- autosparepart_backend/src/main/java/com/irmazda/autosparepart/repository/ProductImageRepository.java
+++ autosparepart_backend/src/main/java/com/irmazda/autosparepart/repository/ProductImageRepository.java
@@ -1,10 +1,17 @@
 package com.irmazda.autosparepart.repository;
 
+import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.irmazda.autosparepart.entity.ProductImage;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface ProductImageRepository extends JpaRepository<ProductImage, UUID> {
+
+  @Query(value = "SELECT * FROM product_images WHERE product_id := productId", nativeQuery = true)
+  Optional<List<ProductImage>> findByProductId(@Param("productId") UUID productId);
 }
